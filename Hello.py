9fea578fd006a4fece09a1a9b7a555307b21ecc5
danilo2328 <danilo.depascalis@acus.eu>
2025-04-07 17:42:46 +0200

    Add files via upload

---
+++ Hello.py
@@ -0,0 +1,13 @@
+#esercizio 1
+print("hello")
+
+#esercizio 1.1 con variabile
+nome = "Alice"
+età = 25
+print("il mio nome è", nome, "e ho", età, "anni")
+
+#esercizio 1.2 con input
+nome = input("inserisci il tuo nome: ")
+età = int(input("inserisci la tua età: "))
+print("Ciao, ", nome, "di anni ", età, "! Benvenuto in python!")
+
---
+++ Liste.py
@@ -0,0 +1,30 @@
+numeri = [1,2,3,4,5]
+nomi = ["Alice", "Bob", "Charlie"]
+misto = [1, "due", True, 4.5]
+
+#accedere agli elementi della lista
+print(numeri[0])
+print(numeri[2])
+
+#modificare un valore nella lista
+numeri[2] = 10
+
+
+
+#metodi sulle liste
+numeri = [7,2,6,8,124]
+numeri.append(25)
+print(numeri)
+
+numeri.sort()
+print(numeri)
+
+numeri[0]= 20
+print(numeri)
+
+#inserire dati in una lista
+lista_dati3 = []
+inserimento = float(input("inserisci un numero"))
+
+lista_dati3.append(inserimento)
+print(lista_dati3)
