6e9a7cceb8c259747a4f2b0f2638f0633a2fb5c5
danilo2328 <danilo.depascalis@acus.eu>
2025-04-08 17:57:49 +0200

    Delete Esercizio3.1_080425.py

--- Esercizio3.1_080425.py
+++
@@ -1,9 +0,0 @@
-#Slide 81 - Punto1
-# Scrivi un sistema che prende in input un numero e stampa "Pari" se il numero è pari e "Dispari" se il numero è dispari.
-
-print("Il tuo numero è un numero pari?")
-numero = int(input("Inserisci un numero intero "))
-if numero%2==0:
-    print(numero, "è un numero pari")
-else:
-    print(numero, "è un numero dispari")
